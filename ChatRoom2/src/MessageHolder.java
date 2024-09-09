e2853fa8bc3efa79273979fafd25668b51c23428
SKRaua <1035013720@qq.com>
2024-09-09 21:43:20 +0800

    t991

---
+++ ChatRoom2/src/MessageHolder.java
@@ -0,0 +1,12 @@
+// 封装类来保存信息
+class MessageHolder {
+    private String message;
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
